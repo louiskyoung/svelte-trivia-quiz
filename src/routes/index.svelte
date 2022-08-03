0b974abeba2a7d0d5430827cda784d26bddd6076
Louis Young <cool.young0420@gmail.com>
2022-08-03 11:09:19 +0900

    add: home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-between">
+	<h1 class="text-3xl font-bold text-center">Welcome to the Trivia Challenge!</h1>
+	<p class="text-2xl text-center">You will be presented with 10 True or False questions.</p>
+	<p class="text-2xl text-center">Can you score 100%?</p>
+	<a
+		class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none"
+		href="/1">Let's begin!</a
+	>
+</div>
+
+<style>
+	div {
+		min-height: calc(99vh - 12em);
+	}
+	a {
+		width: fit-content;
+	}
+</style>
