ca273332fd4941a3055d54836cee9013c29ea2af
Louis Young <cool.young0420@gmail.com>
2022-08-03 11:09:02 +0900

    add: tailwind css

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { sass } from 'svelte-preprocess-sass';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: {
+		...preprocess(),
+		style: sass()
+	},
 
 	kit: {
 		adapter: adapter()
