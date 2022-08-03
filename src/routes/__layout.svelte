fe5240bc8502d6b786e399c07aee8c3618ba682b
Louis Young <cool.young0420@gmail.com>
2022-08-03 11:09:15 +0900

    add: home page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.scss';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		width: 480px;
+		margin: 3em calc(50vw - 240px);
+		padding: 3em;
+		border: 1px solid black;
+	}
+</style>
