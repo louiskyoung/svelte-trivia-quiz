f4161ffa193ab566776dc75be8b3bda545606285
Louis Young <cool.young0420@gmail.com>
2022-08-04 21:05:26 +0900

    add: error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+	/** @type {import('@sveltejs/kit').Load} */
+	export function load({ error, status }: { error: Error; status: string }) {
+		return {
+			props: {
+				status,
+				error
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let status: string;
+	export let error: Error;
+</script>
+
+<div class="page-container flex flex-col items-center justify-center">
+	<h1 class="text-3xl font-bold">Error {status}: {error.message}</h1>
+</div>
